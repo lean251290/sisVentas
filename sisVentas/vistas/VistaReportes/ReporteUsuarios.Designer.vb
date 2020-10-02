ca7a5280c71303791e2c23b6e7b3a3dbca9bff8c
Desireé <Desire@DESKTOP-ACAFJ4P>
2020-10-02 18:29:12 -0300

    creacion de reportes usuario

---
+++ sisVentas/vistas/VistaReportes/ReporteUsuarios.Designer.vb
@@ -0,0 +1,102 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class ReporteUsuarios
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.LabelAgregarCliente = New System.Windows.Forms.Label()
+        Me.PictureBox2 = New System.Windows.Forms.PictureBox()
+        Me.PanelReportes = New System.Windows.Forms.Panel()
+        Me.Panel1.SuspendLayout()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Panel1
+        '
+        Me.Panel1.Controls.Add(Me.LabelAgregarCliente)
+        Me.Panel1.Controls.Add(Me.PictureBox2)
+        Me.Panel1.Dock = System.Windows.Forms.DockStyle.Top
+        Me.Panel1.Location = New System.Drawing.Point(0, 0)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(680, 50)
+        Me.Panel1.TabIndex = 0
+        '
+        'LabelAgregarCliente
+        '
+        Me.LabelAgregarCliente.AutoSize = True
+        Me.LabelAgregarCliente.BackColor = System.Drawing.Color.Transparent
+        Me.LabelAgregarCliente.Dock = System.Windows.Forms.DockStyle.Right
+        Me.LabelAgregarCliente.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.LabelAgregarCliente.ForeColor = System.Drawing.SystemColors.ButtonFace
+        Me.LabelAgregarCliente.Location = New System.Drawing.Point(490, 0)
+        Me.LabelAgregarCliente.Name = "LabelAgregarCliente"
+        Me.LabelAgregarCliente.Size = New System.Drawing.Size(190, 25)
+        Me.LabelAgregarCliente.TabIndex = 7
+        Me.LabelAgregarCliente.Text = "Reportes Usuarios"
+        Me.LabelAgregarCliente.TextAlign = System.Drawing.ContentAlignment.TopCenter
+        '
+        'PictureBox2
+        '
+        Me.PictureBox2.Dock = System.Windows.Forms.DockStyle.Left
+        Me.PictureBox2.Image = Global.sisVentas.My.Resources.Resources.close
+        Me.PictureBox2.Location = New System.Drawing.Point(0, 0)
+        Me.PictureBox2.Name = "PictureBox2"
+        Me.PictureBox2.Size = New System.Drawing.Size(55, 50)
+        Me.PictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage
+        Me.PictureBox2.TabIndex = 6
+        Me.PictureBox2.TabStop = False
+        '
+        'PanelReportes
+        '
+        Me.PanelReportes.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.PanelReportes.BackColor = System.Drawing.Color.FromArgb(CType(CType(0, Byte), Integer), CType(CType(40, Byte), Integer), CType(CType(40, Byte), Integer))
+        Me.PanelReportes.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.PanelReportes.Location = New System.Drawing.Point(39, 107)
+        Me.PanelReportes.Name = "PanelReportes"
+        Me.PanelReportes.Size = New System.Drawing.Size(600, 530)
+        Me.PanelReportes.TabIndex = 7
+        '
+        'ReporteUsuarios
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.FromArgb(CType(CType(0, Byte), Integer), CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer))
+        Me.ClientSize = New System.Drawing.Size(680, 680)
+        Me.Controls.Add(Me.PanelReportes)
+        Me.Controls.Add(Me.Panel1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.Name = "ReporteUsuarios"
+        Me.Text = "Form3"
+        Me.Panel1.ResumeLayout(False)
+        Me.Panel1.PerformLayout()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents PictureBox2 As PictureBox
+    Friend WithEvents LabelAgregarCliente As Label
+    Friend WithEvents PanelReportes As Panel
+End Class
---
+++ sisVentas/vistas/VistaReportes/ReporteUsuarios.vb
@@ -0,0 +1,3 @@
+﻿Public Class ReporteUsuarios
+
+End Class
