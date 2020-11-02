0ca7f5fc2de4a44665bcb8fcfa8988d1a9503d71
lean251290 <lean251290@gmail.com>
2020-11-02 14:33:21 -0300

    actualizacion de la vista clente, validacion de campos

--- sisVentas/vistas/VistaCliente/AgregarCliente.vb
+++ sisVentas/vistas/VistaCliente/AgregarCliente.vb
@@ -2,8 +2,8 @@
 Public Class AgregarCliente
 
     Private Sub BtnAgregarCliente_Click(sender As Object, e As EventArgs) Handles BtnAgregarCliente.Click
-        If (TNombreCliente.Text = "" Or TApellidoCliente.Text = "" Or TEmailCliente.Text = "" Or
-            TDniCliente.Text = "" Or TDireccionCliente.Text = "" Or TTelefonoCliente.Text = "") Then
+        If (TNombreCliente.Text = "Ingrese Nombre" Or TApellidoCliente.Text = "Ingrese Apellido" Or TEmailCliente.Text = "Ingrese E-mail" Or
+            TDniCliente.Text = "Ingrese D.N.I." Or TDireccionCliente.Text = "Ingrese Dirección" Or TTelefonoCliente.Text = "Ingrese Teléfono") Then
             FrmRellenarCampos.Show()
         Else
             Dim cliente As New Cliente(TNombreCliente.Text, TApellidoCliente.Text, TDniCliente.Text, TEmailCliente.Text, TDireccionCliente.Text, TTelefonoCliente.Text)
@@ -74,9 +74,16 @@ Public Class AgregarCliente
         Dim mail As String
         Dim cli As New Cliente
         mail = "^([\w-]+\.)*?[\w-]+@[\w-]+\.([\w-]+\.)*?[\w]+$"
+        If TEmailCliente.Text = Nothing Then
+            TEmailCliente.Text = "Ingrese E-mail"
+            TEmailCliente.ForeColor = Color.DarkSlateGray
+            Exit Sub
+        End If
 
         If Not Regex.IsMatch(TEmailCliente.Text, mail) Then
             FrmErrorMail.Show()
+            TEmailCliente.Text = "Ingrese E-mail"
+            TEmailCliente.ForeColor = Color.DarkSlateGray
         ElseIf cli.VerificarEmail(TEmailCliente.Text) Then
             LabelEmailCliente.Visible = True
             TEmailCliente.Text = ""
@@ -93,18 +100,25 @@ Public Class AgregarCliente
         Dim cantidad As Integer
         Dim cliente As New Cliente
         cantidad = Len(TDniCliente.Text)
-        If cantidad < 8 Then
+        If TDniCliente.Text = Nothing Then
+            TDniCliente.Text = "Ingrese D.N.I."
+            TDniCliente.ForeColor = Color.DarkSlateGray
+        ElseIf cantidad < 8 Then
             FrmValidarDni.Show()
-            TDniCliente.Text = ""
+            TDniCliente.Text = "Ingrese D.N.I."
+            TDniCliente.ForeColor = Color.DarkSlateGray
         ElseIf cantidad > 8 Then
             FrmValidarDni.Show()
-            TDniCliente.Text = ""
+            TDniCliente.Text = "Ingrese D.N.I."
+            TDniCliente.ForeColor = Color.DarkSlateGray
         ElseIf cliente.VerificarDNI(TDniCliente.Text) Then
             LabelDniCliente.Visible = True
-            TDniCliente.Text = ""
+            TDniCliente.Text = "Ingrese D.N.I."
+            TDniCliente.ForeColor = Color.DarkSlateGray
         Else
             LabelDniCliente.Visible = False
         End If
+
     End Sub
 
 
@@ -131,12 +145,29 @@ Public Class AgregarCliente
         LabelEmailCliente.Visible = False
         LabelDireccionCli.Visible = False
         LabelTelCli.Visible = False
+        TDniCliente.Text = "Ingrese D.N.I."
+        TDniCliente.ForeColor = Color.DarkSlateGray
+        TNombreCliente.Text = "Ingrese Nombre"
+        TNombreCliente.ForeColor = Color.DarkSlateGray
+        TApellidoCliente.Text = "Ingrese Apellido"
+        TApellidoCliente.ForeColor = Color.DarkSlateGray
+        TEmailCliente.Text = "Ingrese E-mail"
+        TEmailCliente.ForeColor = Color.DarkSlateGray
+        TDireccionCliente.Text = "Ingrese Dirección"
+        TDireccionCliente.ForeColor = Color.DarkSlateGray
+        TTelefonoCliente.Text = "Ingrese Teléfono"
+        TTelefonoCliente.ForeColor = Color.DarkSlateGray
     End Sub
 
 
 
     Private Sub TDireccionCliente_LostFocus(sender As Object, e As EventArgs) Handles TDireccionCliente.LostFocus
         Dim direccion As String
+        If TDireccionCliente.Text = Nothing Then
+            TDireccionCliente.Text = "Ingrese Dirección"
+            TDireccionCliente.ForeColor = Color.DarkSlateGray
+            Exit Sub
+        End If
         direccion = "^[A-z\s]+(\d{1,5})"
         If Not Regex.IsMatch(TDireccionCliente.Text, direccion) Then
             LabelDireccionCli.Visible = True
@@ -150,6 +181,11 @@ Public Class AgregarCliente
     Private Sub TTelefonoCliente_LostFocus(sender As Object, e As EventArgs) Handles TTelefonoCliente.LostFocus
         Dim telefono As String
         telefono = "^\d{1,3}"
+        If TTelefonoCliente.Text = Nothing Then
+            TTelefonoCliente.Text = "Ingrese Teléfono"
+            TTelefonoCliente.ForeColor = Color.DarkSlateGray
+            Exit Sub
+        End If
         If Not Regex.IsMatch(TTelefonoCliente.Text, telefono) Then
             LabelTelCli.Visible = True
             TTelefonoCliente.Text = ""
@@ -159,6 +195,12 @@ Public Class AgregarCliente
     End Sub
 
     Private Sub TNombreCliente_LostFocus(sender As Object, e As EventArgs) Handles TNombreCliente.LostFocus
+        If TNombreCliente.Text = Nothing Then
+            TNombreCliente.Text = "Ingrese Nombre"
+            TNombreCliente.ForeColor = Color.DarkSlateGray
+            Exit Sub
+        End If
+
         If TNombreCliente.Text <> "" Then
             Dim texto As String()
             Dim nombreCompleto As String = ""
@@ -172,6 +214,13 @@ Public Class AgregarCliente
     End Sub
 
     Private Sub TApellidoCliente_LostFocus(sender As Object, e As EventArgs) Handles TApellidoCliente.LostFocus
+        If TApellidoCliente.Text = Nothing Then
+            TApellidoCliente.Text = "Ingrese Apellido"
+            TApellidoCliente.ForeColor = Color.DarkSlateGray
+            Exit Sub
+        End If
+
+
         If TApellidoCliente.Text <> "" Then
             Dim texto As String()
             Dim nombreCompleto As String = ""
@@ -187,5 +236,71 @@ Public Class AgregarCliente
     Private Sub BtnCancelarCliente_Click(sender As Object, e As EventArgs) Handles BtnCancelarCliente.Click
         Me.Close()
         PanelAdmin.Show()
+    End Sub
+
+    Private Sub TDniCliente_TextChanged(sender As Object, e As EventArgs) Handles TDniCliente.TextChanged
+
+    End Sub
+
+    Private Sub TDniCliente_GotFocus(sender As Object, e As EventArgs) Handles TDniCliente.GotFocus
+        If TDniCliente.Text = "Ingrese D.N.I." Then
+            TDniCliente.ForeColor = Color.Black
+            TDniCliente.Text = ""
+        End If
+    End Sub
+
+    Private Sub TNombreCliente_TextChanged(sender As Object, e As EventArgs) Handles TNombreCliente.TextChanged
+
+    End Sub
+
+    Private Sub TNombreCliente_GotFocus(sender As Object, e As EventArgs) Handles TNombreCliente.GotFocus
+        If TNombreCliente.Text = "Ingrese Nombre" Then
+            TNombreCliente.ForeColor = Color.Black
+            TNombreCliente.Text = ""
+        End If
+    End Sub
+
+    Private Sub TApellidoCliente_TextChanged(sender As Object, e As EventArgs) Handles TApellidoCliente.TextChanged
+
+    End Sub
+
+    Private Sub TApellidoCliente_GotFocus(sender As Object, e As EventArgs) Handles TApellidoCliente.GotFocus
+        If TApellidoCliente.Text = "Ingrese Apellido" Then
+            TApellidoCliente.ForeColor = Color.Black
+            TApellidoCliente.Text = ""
+        End If
+    End Sub
+
+    Private Sub TEmailCliente_TextChanged(sender As Object, e As EventArgs) Handles TEmailCliente.TextChanged
+
+    End Sub
+
+    Private Sub TEmailCliente_GotFocus(sender As Object, e As EventArgs) Handles TEmailCliente.GotFocus
+        If TEmailCliente.Text = "Ingrese E-mail" Then
+            TEmailCliente.ForeColor = Color.Black
+            TEmailCliente.Text = ""
+        End If
+    End Sub
+
+    Private Sub TDireccionCliente_TextChanged(sender As Object, e As EventArgs) Handles TDireccionCliente.TextChanged
+
+    End Sub
+
+    Private Sub TDireccionCliente_GotFocus(sender As Object, e As EventArgs) Handles TDireccionCliente.GotFocus
+        If TDireccionCliente.Text = "Ingrese Dirección" Then
+            TDireccionCliente.ForeColor = Color.Black
+            TDireccionCliente.Text = ""
+        End If
+    End Sub
+
+    Private Sub TTelefonoCliente_TextChanged(sender As Object, e As EventArgs) Handles TTelefonoCliente.TextChanged
+
+    End Sub
+
+    Private Sub TTelefonoCliente_GotFocus(sender As Object, e As EventArgs) Handles TTelefonoCliente.GotFocus
+        If TTelefonoCliente.Text = "Ingrese Teléfono" Then
+            TTelefonoCliente.ForeColor = Color.Black
+            TTelefonoCliente.Text = ""
+        End If
     End Sub
 End Class
